b67407aae9fca16920e83d38898c84d7ee7b9e7d
gaaliciA1990 <32443614+gaaliciA1990@users.noreply.github.com>
2022-04-27 19:05:11 -0700

    Update project with Professor updates. Updates MongooseDB/createDB/createToDoSampleData

--- expressServer/metaverse_api_calls/decentraland.ts
+++ expressServer/metaverse_api_calls/decentraland.ts
@@ -1,8 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request');
+import * as path from 'path';
+import * as express from 'express';
+import * as url from 'url';
+import * as bodyParser from 'body-parser';
 
 // Calls the Decentraland metaverse api to pull parcel/estate data
 class Decentraland {
+
 
 }
