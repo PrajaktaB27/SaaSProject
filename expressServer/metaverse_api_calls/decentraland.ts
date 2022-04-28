a80122a97441323e5332878df67b51e26deb416d
gaaliciA1990 <32443614+gaaliciA1990@users.noreply.github.com>
2022-04-27 17:49:05 -0700

    Updated decentraland file name

---
+++ expressServer/metaverse_api_calls/decentraland.ts
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+// Calls the Decentraland metaverse api to pull parcel/estate data
+class Decentraland {
+
+}
