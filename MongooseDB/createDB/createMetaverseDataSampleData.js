8ebf42529a081574794e0fd027fc1b0ed8fafe7d
Francis Kogge <koggefa@gmail.com>
2022-05-03 16:59:57 -0700

    added some more tiles to the population script

--- MongooseDB/createDB/createMetaverseDataSampleData.js
+++ MongooseDB/createDB/createMetaverseDataSampleData.js
@@ -6,79 +6,159 @@ tilesCollection.remove({})
 tilesCollection.insert(
 	{
 		tileId: "-150,150",
-        x: -150,
-        y: 150,
-        updatedAt: 1543335084,
-        type: "district",
-        top: true,
-        left: false,
-        topLeft: false,
-        estateId: 1186,
-        owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
-        tokenId: "115792089237316195423570985008687907802227629627499794519951392893147897921686"
+                x: -150,
+                y: 150,
+                updatedAt: 1543335084,
+                type: "district",
+                top: true,
+                left: false,
+                topLeft: false,
+                estateId: 1186,
+                owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
+                tokenId: "115792089237316195423570985008687907802227629627499794519951392893147897921686"
 	}
 )
 
 tilesCollection.insert(
 	{
 		tileId: "-150,149",
-        x: -150,
-        y: 149,
-        updatedAt: 1543335084,
-        type: "district",
-        top: true,
-        left: false,
-        topLeft: false,
-        estateId: 1186,
-        owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
-        tokenId: "115792089237316195423570985008687907802227629627499794519951392893147897921685"
+                x: -150,
+                y: 149,
+                updatedAt: 1543335084,
+                type: "district",
+                top: true,
+                left: false,
+                topLeft: false,
+                estateId: 1186,
+                owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
+                tokenId: "115792089237316195423570985008687907802227629627499794519951392893147897921685"
 	}
 )
 
 tilesCollection.insert(
 	{
 		tileId: "-150,148",
-        x: -150,
-        y: 148,
-        updatedAt: 1543335084,
-        type: "district",
-        top: true,
-        left: false,
-        topLeft: false,
-        estateId: 1186,
-        owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
-        tokenId: "115792089237316195423570985008687907802227629627499794519951392893147897921684"
+                x: -150,
+                y: 148,
+                updatedAt: 1543335084,
+                type: "district",
+                top: true,
+                left: false,
+                topLeft: false,
+                estateId: 1186,
+                owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
+                tokenId: "115792089237316195423570985008687907802227629627499794519951392893147897921684"
 	}
 )
 
 tilesCollection.insert(
 	{
 		id: "-150,79",
-        x: -150,
-        y: 79,
-        updatedAt: 1543335274,
-        type: "district",
-        top: true,
-        left: false,
-        topLeft: false,
-        estateId: 1183,
-        owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
-        tokenId: "115792089237316195423570985008687907802227629627499794519951392893147897921615"
+                x: -150,
+                y: 79,
+                updatedAt: 1543335274,
+                type: "district",
+                top: true,
+                left: false,
+                topLeft: false,
+                estateId: 1183,
+                owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
+                tokenId: "115792089237316195423570985008687907802227629627499794519951392893147897921615"
 	}
 )
 
 tilesCollection.insert(
 	{
-        id: "-150,78",
-        x: -150,
-        y: 78,
-        updatedAt: 1543335274,
-        type: "district",
-        top: true,
-        left: false,
-        topLeft: false,
-        estateId: 1183,
-        owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
-        tokenId: "115792089237316195423570985008687907802227629627499794519951392893147897921614"
-	}
+                id: "-150,78",
+                x: -150,
+                y: 78,
+                updatedAt: 1543335274,
+                type: "district",
+                top: true,
+                left: false,
+                topLeft: false,
+                estateId: 1183,
+                owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
+                tokenId: "115792089237316195423570985008687907802227629627499794519951392893147897921614"
+        }
+)
+
+tilesCollection.insert(
+        {
+                id: "-91,150",
+                x: -91,
+                y: 150,
+                updatedAt: 1582548358,
+                type: "district",
+                top: true,
+                left: true,
+                topLeft: true,
+                estateId: 1191,
+                owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
+                tokenId: "115792089237316195423570985008687907822304289275835163864290494731622222397590"
+        }
+)
+
+tilesCollection.insert(
+        {
+                id: "-91,149",
+                x: -91,
+                y: 149,
+                updatedAt: 1582548358,
+                type: "district",
+                top: true,
+                left: true,
+                topLeft: true,
+                estateId: 1191,
+                owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
+                tokenId: "115792089237316195423570985008687907822304289275835163864290494731622222397589"
+        }
+)
+
+tilesCollection.insert(
+        {
+                id: "-91,148",
+                x: -91,
+                y: 148,
+                updatedAt: 1582548358,
+                type: "district",
+                top: true,
+                left: true,
+                topLeft: true,
+                estateId: 1191,
+                owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
+                tokenId: "115792089237316195423570985008687907822304289275835163864290494731622222397588"
+        }
+)
+
+tilesCollection.insert(
+        {
+                id: "-91,147",
+                x: -91,
+                y: 147,
+                updatedAt: 1582548358,
+                type: "district",
+                top: true,
+                left: true,
+                topLeft: true,
+                estateId: 1191,
+                owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
+                tokenId: "115792089237316195423570985008687907822304289275835163864290494731622222397587"
+        }
+)
+
+tilesCollection.insert(
+        {
+                id: "-91,146",
+                x: -91,
+                y: 146,
+                updatedAt: 1582548358,
+                type: "district",
+                top: true,
+                left: true,
+                topLeft: true,
+                estateId: 1191,
+                owner: "0xa65be351527ebcf8c1707d1e444dac38b41a5faf",
+                tokenId: "115792089237316195423570985008687907822304289275835163864290494731622222397586"
+        }
 )
