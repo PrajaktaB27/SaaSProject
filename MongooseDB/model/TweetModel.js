beda2b4fb055444b3ad7089f9be97edc7f2c6557
Francis Kogge <koggefa@gmail.com>
2022-05-03 16:58:37 -0700

    added TweetModel.js

---
+++ MongooseDB/model/TweetModel.js
@@ -0,0 +1,40 @@
+"use strict";
+exports.__esModule = true;
+exports.TweetModel = void 0;
+var Mongoose = require("mongoose");
+var DataAccess_1 = require("../DataAccess");
+var mongooseConnection = DataAccess_1.DataAccess.mongooseConnection;
+var mongooseObj = DataAccess_1.DataAccess.mongooseInstance;
+var TweetModel = /** @class */ (function () {
+    function TweetModel() {
+        this.createSchema();
+        this.createModel();
+    }
+    TweetModel.prototype.createSchema = function () {
+        this.schema = new Mongoose.Schema({
+            tweetId: String,
+            userID: String,
+            tweetDescription: String,
+            tweetDate: Date
+        }, { collection: 'tweets' });
+    };
+    TweetModel.prototype.createModel = function () {
+        this.model = mongooseConnection.model("Tweet", this.schema);
+    };
+    TweetModel.prototype.retrieveTweetById = function (response, filter) {
+        var query = this.model.findOne(filter);
+        query.exec(function (err, tile) {
+            console.log(tile);
+            response.json(tile);
+        });
+    };
+    TweetModel.prototype.retrieveAllTweets = function (response, filter) {
+        var query = this.model.find(filter);
+        query.exec(function (err, tileList) {
+            console.log(tileList);
+            response.json(tileList);
+        });
+    };
+    return TweetModel;
+}());
+exports.TweetModel = TweetModel;
