c737a624efb23b1cab5c000752523d1b8f8e88e4
Francis Kogge <koggefa@gmail.com>
2022-05-03 15:56:38 -0700

    added cards for metaverses and due diligence in index.html, added css style sheet and added click funciton in script.js

--- MongooseDB/pages/script.js
+++ MongooseDB/pages/script.js
@@ -111,3 +111,12 @@ function populateListItems(elementId, list) {
 	}
 	$("#" + elementId).append(newElement);
 }
+
+function displayBuyerSellerInfo(isBuyer) {
+	if (isBuyer) {
+		alert("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+	} else {
+		alert("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+	}
+	
+}
