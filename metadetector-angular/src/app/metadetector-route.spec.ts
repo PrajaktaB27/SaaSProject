4f1ec075ec71f1ffbfff61152f8abfed1eff0d49
Alicia Garcia <garcia.alicia1990@gmail.com>
2022-05-13 11:54:08 -0700

    Created router class

---
+++ metadetector-angular/src/app/metadetector-route.spec.ts
@@ -0,0 +1,7 @@
+import { MetadetectorRoute } from './metadetector-route';
+
+describe('MetadetectorRoute', () => {
+  it('should create an instance', () => {
+    expect(new MetadetectorRoute()).toBeTruthy();
+  });
+});
