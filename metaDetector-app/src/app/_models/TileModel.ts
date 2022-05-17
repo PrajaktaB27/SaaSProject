25d8360278abc6b7b855342ef878e97bba728ecc
Francis Kogge <koggefa@gmail.com>
2022-05-16 22:38:54 -0700

    added TileModel for angular models

---
+++ metaDetector-app/src/app/_models/TileModel.ts
@@ -0,0 +1,13 @@
+export interface TileModel {
+    tileId: string,
+    x: number,
+    y: number,
+    updatedAt: number,
+    type: string,
+    top: boolean,
+    left: boolean,
+    topLeft: boolean,
+    estateId: number,
+    owner: string,
+    tokenId: string 
+};
