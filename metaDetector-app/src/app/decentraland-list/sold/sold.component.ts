560303a1fa7d5e43bdd534246bc7d7d12ebe446e
Francis Kogge <koggefa@gmail.com>
2022-05-16 22:51:26 -0700

    rendering individual sold tiles in decentraland page, in sold component

--- metaDetector-app/src/app/decentraland-list/sold/sold.component.ts
+++ metaDetector-app/src/app/decentraland-list/sold/sold.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MetadetectorApiService } from 'src/app/metadetector-api.service';
+import { TileModel } from 'src/app/_models/TileModel';
 
 @Component({
   selector: 'app-sold',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SoldComponent implements OnInit {
 
-  constructor() { }
+  listOfTileLists: Array<TileModel[]> = [];
+  static tileType: string = 'district'; // change once we get tiles with type=owned in DB
+  //static tileType: string = 'owned'
+
+  constructor(private metaApiService: MetadetectorApiService) { }
 
   ngOnInit(): void {
+    this.metaApiService.getTileByType(SoldComponent.tileType).subscribe( (result: TileModel[]) => {
+      for (var i = 0; i < result.length; i++) {
+        if (i % 3 == 0) {
+          this.listOfTileLists.push([] as TileModel[]);
+        }
+        
+        this.listOfTileLists[this.listOfTileLists.length - 1].push(result[i]);
+      }
+      console.log(this.listOfTileLists);
+    });
   }
 
 }
