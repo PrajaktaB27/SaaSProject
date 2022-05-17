35d85bec58a89ab640968a97d52556dbd6019026
khometit <averydinh1996@gmail.com>
2022-05-17 16:44:04 -0700

    Added code to Tweet component to work with Directives

--- metaDetector-app/src/app/meta-home/tweet/tweet.component.ts
+++ metaDetector-app/src/app/meta-home/tweet/tweet.component.ts
@@ -10,6 +10,19 @@ import { MetadetectorApiService } from 'src/app/metadetector-api.service';
 export class TweetComponent implements OnInit {
 
   results : Array<TweetModel>= [];
+  counter(i: number){
+    return new Array(i);
+  }
+  index: number = 0;
+
+  getTweet():TweetModel{
+    if(this.index >= this.results.length){
+      this.index = 0;
+    }
+    let i = this.index;
+    this.index++;
+    return this.results[i];
+  }
 
   constructor(private MetaService: MetadetectorApiService) { }
 
