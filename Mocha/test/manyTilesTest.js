05f7f776a9bf2b01e5b916eb75be7bc762e21fb7
khometit <averydinh1996@gmail.com>
2022-05-19 21:11:30 -0700

    Updated test

--- Mocha/test/manyTilesTest.js
+++ Mocha/test/manyTilesTest.js
@@ -13,10 +13,10 @@ describe('Test Tiles result', function () {
 //	this.timeout(15000);
 
     let hostURL = "http://localhost:8080";
-    let path = "/app/tile/estate/1186";
+    let path = "/app/tile/estate/4893";
 	var requestResult;
 	var response;
-		 
+
     before(function (done) {
         chai.request(hostURL)
 			.get(path)
@@ -35,25 +35,26 @@ describe('Test Tiles result', function () {
         expect(response).to.be.json;
 		expect(response.body).to.have.length.above(2);
 		expect(response).to.have.headers;
+		expect(response.body).to.be.an('array');
     });
     
 	it('The first entry in the array has known properties', function(){
 	    expect(requestResult[0]).to.include.all.keys('tileId', 'type', 'updatedAt');
 	    expect(requestResult[0]).to.have.property('_id');
-        expect(requestResult[0]).to.have.property('estateId', 1186);
+        expect(requestResult[0]).to.have.property('estateId', 4893);
 		expect(response.body[0]).to.have.deep.property('tokenId');
 		expect(response.body).to.not.be.a.string;
 	});
 
-	it('The elements in the array have the expecte properties', function(){
+	it('The elements in the array have the expected properties with defined types', function(){
 		expect(response.body).to.satisfy(
 			function (body) {
 				for (var i = 0; i < body.length; i++) {
-					expect(body[i]).to.have.property('_id');
-					expect(body[i]).to.have.property('tileId');
-					expect(body[i]).to.have.property('updatedAt');
+					expect(body[i]).to.have.property('_id').that.is.a('string');;
+					expect(body[i]).to.have.property('tileId').that.is.a('string');;
+					expect(body[i]).to.have.property('updatedAt').that.is.a('number');;
 					expect(body[i]).to.have.property('type').that.is.a('string');
-                    expect(body[i]).to.have.property('estateId', 1186).that.is.a('number');
+                    expect(body[i]).to.have.property('estateId', 4893).that.is.a('number');
 					expect(body[i]).to.have.property('owner').that.is.a('string');
                     expect(body[i]).to.have.property('tokenId').that.is.a('string');
 				}
