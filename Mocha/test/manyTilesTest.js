1e023f2d449656525542a4741bf1d5a2d80bf728
Francis Kogge <koggefa@gmail.com>
2022-06-01 23:42:46 -0700

    changed server requests to azure url in mocha tests

--- Mocha/test/manyTilesTest.js
+++ Mocha/test/manyTilesTest.js
@@ -10,43 +10,43 @@ var http = require('http');
 chai.use(chaiHttp);
 
 describe('Test Tiles result', function () {
-//	this.timeout(15000);
+	//	this.timeout(15000);
 
-    let hostURL = "http://localhost:8080";
-    let path = "/app/tile/estate/4893";
+	let hostURL = "https://metadetector.azurewebsites.net";
+	let path = "/app/tile/estate/4893";
 	var requestResult;
 	var response;
 
-    before(function (done) {
-        chai.request(hostURL)
+	before(function (done) {
+		chai.request(hostURL)
 			.get(path)
 			.end(function (err, res) {
 				requestResult = res.body;
 				response = res;
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
+				expect(err).to.be.null;
+				expect(res).to.have.status(200);
 				done();
 			});
-        });
-    
-    it('Should return an array object with more than 1 object', function (){
+	});
+
+	it('Should return an array object with more than 1 object', function () {
 		expect(response).to.have.status(200);
 		//Expect the response body to be json
-        expect(response).to.be.json;
+		expect(response).to.be.json;
 		expect(response.body).to.have.length.above(2);
 		expect(response).to.have.headers;
 		expect(response.body).to.be.an('array');
-    });
-    
-	it('The first entry in the array has known properties', function(){
-	    expect(requestResult[0]).to.include.all.keys('tileId', 'type', 'updatedAt');
-	    expect(requestResult[0]).to.have.property('_id');
-        expect(requestResult[0]).to.have.property('estateId', 4893);
+	});
+
+	it('The first entry in the array has known properties', function () {
+		expect(requestResult[0]).to.include.all.keys('tileId', 'type', 'updatedAt');
+		expect(requestResult[0]).to.have.property('_id');
+		expect(requestResult[0]).to.have.property('estateId', 4893);
 		expect(response.body[0]).to.have.deep.property('tokenId');
 		expect(response.body).to.not.be.a.string;
 	});
 
-	it('The elements in the array have the expected properties with defined types', function(){
+	it('The elements in the array have the expected properties with defined types', function () {
 		expect(response.body).to.satisfy(
 			function (body) {
 				for (var i = 0; i < body.length; i++) {
@@ -54,12 +54,12 @@ describe('Test Tiles result', function () {
 					expect(body[i]).to.have.property('tileId').that.is.a('string');;
 					expect(body[i]).to.have.property('updatedAt').that.is.a('number');;
 					expect(body[i]).to.have.property('type').that.is.a('string');
-                    expect(body[i]).to.have.property('estateId', 4893).that.is.a('number');
+					expect(body[i]).to.have.property('estateId', 4893).that.is.a('number');
 					expect(body[i]).to.have.property('owner').that.is.a('string');
-                    expect(body[i]).to.have.property('tokenId').that.is.a('string');
+					expect(body[i]).to.have.property('tokenId').that.is.a('string');
 				}
 				return true;
 			});
-	});	
-	
+	});
+
 });
--- Mocha/test/singleTileTest.js
+++ Mocha/test/singleTileTest.js
@@ -10,50 +10,50 @@ var http = require('http');
 chai.use(chaiHttp);
 
 describe('Test 1 Tile result', function () {
-//	this.timeout(15000);
+	//	this.timeout(15000);
 
-    let hostURL = "http://localhost:8080";
-    let path = "/app/tile/?x=-150&y=150";
+	let hostURL = "https://metadetector.azurewebsites.net";
+	let path = "/app/tile/?x=-150&y=150";
 	var requestResult;
 	var response;
-		 
-    before(function (done) {
-        chai.request(hostURL)
+
+	before(function (done) {
+		chai.request(hostURL)
 			.get(path)
 			.end(function (err, res) {
 				requestResult = res.body;
 				response = res;
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
+				expect(err).to.be.null;
+				expect(res).to.have.status(200);
 				done();
 			});
-        });
-    
-    it('Should return 1 Tile object', function (){
+	});
+
+	it('Should return 1 Tile object', function () {
 		expect(response).to.have.status(200);
 		//Expect the response body to be json
-        expect(response).to.be.json;
+		expect(response).to.be.json;
 		expect(response.body).to.be.a('Object');
 		expect(response).to.have.headers;
-        
-    });
-    
-	it('Should have  known properties', function(){
-	    expect(requestResult).to.include.all.keys('tileId', 'type', 'updatedAt');
-	    expect(requestResult).to.have.property('_id');
-        expect(requestResult).to.have.property('estateId', 1186);
+
+	});
+
+	it('Should have  known properties', function () {
+		expect(requestResult).to.include.all.keys('tileId', 'type', 'updatedAt');
+		expect(requestResult).to.have.property('_id');
+		expect(requestResult).to.have.property('estateId', 1186);
 		expect(response.body).to.have.deep.property('tokenId');
 		expect(response.body).to.not.be.a.string;
 	});
 
-	it('Should have all expected properties with expected types', function(){
-        var theTile = response.body;
-        expect(theTile).to.have.property('_id').that.is.a('string');
-        expect(theTile).to.have.property('tileId').that.is.a('string');
-        expect(theTile).to.have.property('updatedAt').that.is.a('number');
-        expect(theTile).to.have.property('type').that.is.a('string');
-        expect(theTile).to.have.property('estateId', 1186).that.is.a('number');
-        expect(theTile).to.have.property('owner').that.is.a('string');
-        expect(theTile).to.have.property('tokenId').that.is.a('string');	
+	it('Should have all expected properties with expected types', function () {
+		var theTile = response.body;
+		expect(theTile).to.have.property('_id').that.is.a('string');
+		expect(theTile).to.have.property('tileId').that.is.a('string');
+		expect(theTile).to.have.property('updatedAt').that.is.a('number');
+		expect(theTile).to.have.property('type').that.is.a('string');
+		expect(theTile).to.have.property('estateId', 1186).that.is.a('number');
+		expect(theTile).to.have.property('owner').that.is.a('string');
+		expect(theTile).to.have.property('tokenId').that.is.a('string');
 	});
 });
