373cdf173fba98c99801da0fac7101d15e80a26a
khometit <averydinh1996@gmail.com>
2022-06-05 16:29:43 -0700

    Added UserService to Angular app

---
+++ metaDetector-app/src/app/user.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UserModel } from './_models/UserModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  //hostUrl : string = 'https://metadetector.azurewebsites.net/app/';
+  hostUrl : string = 'http://localhost:8080/';
+
+  constructor(private http:HttpClient) { }
+
+  getUserInfo(){
+    return this.http.get<UserModel>(this.hostUrl + 'auth/user/info');
+  }
+
+  addNewFavorite(){
+    //doesn't need to obtain user ID because passport on the backend already keep track of that
+    //Placeholder
+  }
+
+}
